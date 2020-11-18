f657b37ebc663e8f968ba3293491ecf1234ef988
Oluwatobi <oladipupo_tobi@yahoo.com>
2020-11-18 15:44:28 +0100

    fix  some npm  error

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response }  from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@sgtickets/common';
-
+import timeout from 'connect-timeout'
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -23,7 +23,19 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+//var timeout = require('connect-timeout')
 
+// example of using this top-level; note the use of haltOnTimedout
+// after every middleware; it will stop the request flow on a timeout
+app.use(timeout('400s'))
+
+app.use(haltOnTimedout)
+
+// Add your routes here, etc.
+
+function haltOnTimedout (req: Request, res:Response, next: NextFunction) {
+  if (!req.timedout) next()
+}
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
